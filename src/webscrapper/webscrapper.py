fb716215de162fb1388c4de0c1267df88d6b5c29
sgarnica1 <sgarnica1902@gmail.com>
2024-01-02 20:39:29 -0600

    feat: read complete data from table in url

--- src/webscrapper/webscrapper.py
+++ src/webscrapper/webscrapper.py
@@ -1,6 +1,7 @@
 # webscrapper/webscrapper.py
 
 from bs4 import BeautifulSoup
+import pandas as pd
 import requests
 
 def main():
@@ -13,4 +14,14 @@ def main():
     table_titles = table.find_all('th')
 
     table_titles_text = [title.text.strip() for title in table_titles]
-    print(table_titles_text)
+
+    df = pd.DataFrame(columns = table_titles_text)
+
+    table_rows = table.find_all('tr')
+
+    for row in table_rows[1:]:
+        row_data = row.find_all('td')
+        individual_row_data = [data.text.strip() for data in row_data]
+        df.loc[len(df)] = individual_row_data   
+
+    print(df)
